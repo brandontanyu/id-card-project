d224ac054d7360cd4dbdd1e6bb6f0d7596cef77a
Tanyu Brandon <130669586+brandontanyu@users.noreply.github.com>
2023-08-21 15:11:18 +0100

    done with major components of the ID interface

--- lib/main.dart
+++ lib/main.dart
@@ -17,27 +17,75 @@ class NinjaCard extends StatelessWidget {
         backgroundColor: Colors.grey[850],
         elevation: 0.0,
       ),
-      body: const Padding(
-        padding:EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
+      body:Padding(
+        padding:const EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
+            const Center(
+              child: CircleAvatar(
+                backgroundImage: AssetImage("assets/userIcon.png"),
+                radius: 40.0,
+              ),
+            ),
+             Divider(
+              height: 60.0,
+              color: Colors.grey[800],
+            ),
             Text(
-              "Name",
+              "NAME",
               style: TextStyle(
-                color: Colors.grey,
+                color: Colors.grey[100],
                 letterSpacing: 2.0,
               ),
             ),
+            const SizedBox(height: 10.0),
             Text(
               "Tanyu Brandon",
               style: TextStyle(
-                color: Colors.amberAccent,
+                color: Colors.amberAccent[200],
                 letterSpacing: 2.0,
                 fontSize: 20.0,
                 fontWeight: FontWeight.bold,
               ),
             ),
+            const SizedBox(height: 30.0,),
+            Text(
+              "CURRENT NINJA LEVEL",
+              style: TextStyle(
+                color: Colors.grey[100],
+                letterSpacing: 2.0,
+              ),
+            ),
+            const SizedBox(height: 10.0,),
+            Text(
+              "8",
+              style: TextStyle(
+                color: Colors.amberAccent[200],
+                letterSpacing: 2.0,
+                fontSize: 20.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 30.0,),
+            Row(
+               children: [
+                 Icon(
+                   Icons.email,
+                   color: Colors.grey[100],
+                 ),
+                 const SizedBox(width: 5.0,),
+                 Text(
+                   "brandon@gmail.co.uk",
+                   style: TextStyle(
+                     color: Colors.grey[400],
+                     fontSize: 18.0,
+                     letterSpacing: 1.0,
+                   ),
+                 ),
+               ],
+            ),
+
           ],
         ),
       ),
