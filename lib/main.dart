8b2cc4a4bb68f588227ccd183c25c9b28b2c9e12
Tanyu Brandon <130669586+brandontanyu@users.noreply.github.com>
2023-08-21 07:43:46 +0100

    changed appBar and background color

--- lib/main.dart
+++ lib/main.dart
@@ -10,9 +10,12 @@ class NinjaCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return  Scaffold(
+      backgroundColor: Colors.grey[900],
       appBar: AppBar(
         title: const Text("Ninja ID Card"),
         centerTitle: true,
+        backgroundColor: Colors.grey[850],
+        elevation: 0.0,
       ),
     );
   }
