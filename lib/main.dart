4b14a022b553785fa24c0fd06bb38a363af6c058
Tanyu Brandon <130669586+brandontanyu@users.noreply.github.com>
2023-08-21 07:16:45 +0100

    base widget created

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,21 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp());
+void main() => runApp(const MaterialApp(
+  home: NinjaCard(),
+));
+
+class NinjaCard extends StatelessWidget {
+  const NinjaCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      appBar: AppBar(
+        title: const Text("Ninja ID Card"),
+        centerTitle: true,
+      ),
+    );
+  }
+}
+
 
